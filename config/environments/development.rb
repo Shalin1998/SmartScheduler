419dc26df750674828d0eda8dd9fc3ca2cfb9dcb
Shalin Patel <shalin617@gmail.com>
2015-06-19 07:16:12 -0400

    Calendar added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,15 +40,4 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
 
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-          :address              => "smtp.gmail.com",
-          :port                 => 587,
-          :domain               => "gmail.com",
-          :user_name            => "noreply.smartschedule@gmail.com",
-          :password             => "ShalinPatel",
-          :authentication       => :login,
-          :enable_starttls_auto => true
-    }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+        :address              => "smtp.gmail.com",
+        :port                 => 587,
+        :domain               => "gmail.com",
+        :user_name            => "noreply.smartschedule@gmail.com",
+        :password             => "ShalinPatel",
+        :authentication       => :login,
+        :enable_starttls_auto => true
+  }
+
 end
